13552ef032e975b31e849bb564f2d9eb253d7756
billkwon <fd9020@gmail.com>
2018-08-29 11:03:45 +0900

    file reset

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	// console.log('req.user:', req.user);
-	if (!req.user || !req.isAuthenticated()) {
-		// Not logged in yet.
-		res.redirect('/auth/google');
-	} else {
-		res.redirect('/getAlbums');
-	}
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
